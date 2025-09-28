d8bec5e3deb6ea9d66575dd5e700719133b80d54
Matthew Lowry <Lowry-M6@ulster.ac.uk>
2025-09-29 00:58:32 +0100

    updated program code further

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,14 +1,59 @@
-﻿//we will ask the user to input the numbers.
-//two variables which will store the two numbers for the calc.
-Console.WriteLine("Type in the 1st number followed by the enter key");
-int numOne = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Type in the 2nd number followed by the enter key");
-int numTwo = Convert.ToInt32(Console.ReadLine());
+﻿using System.ComponentModel.Design;
+using System.Xml.Serialization;
 
-//the next variable is result, this will contain the sum of numOne and numTwo.
-int result = numOne + numTwo;
-//this next line displays the result
-Console.WriteLine("The addition of both numebers is {0}", result);
-Console.ReadKey();
+CalculatorApp();
 
+void CalculatorApp()
+{
+    //declare the variables and initialise them to 0
+    int numOne = 0;
+    int numTwo = 0;
+    int result = 0;
+    int choice = 0;
+    //we will ask the user to input the numbers.
+    //two variables which will store the two numbers for the calc.
+    Console.WriteLine("Type in the 1st number followed by the enter key");
+    numOne = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Type in the 2nd number followed by the enter key");
+    numTwo = Convert.ToInt32(Console.ReadLine());
+
+    //make a loop to make the relevant decision
+    //perform the requested maths operation
+    Console.WriteLine("Choose and option from the following list:");
+    Console.WriteLine("1 - add");
+    Console.WriteLine("2 - subtract");
+    Console.WriteLine("3 - divide");
+    Console.WriteLine("4 - multiply");
+    //convert string to int
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    //if statment to perform selected maths op
+    if (choice == 1)
+    {
+        result = numOne + numTwo;
+        Console.WriteLine($"Adding {numOne} and {numTwo} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = numOne - numTwo;
+        Console.WriteLine($"Subtracting {numOne} and {numTwo} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = numOne / numTwo;
+        Console.WriteLine($"Dividing {numOne} and {numTwo} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = numOne * numTwo;
+        Console.WriteLine($"Multiplying {numOne} and {numTwo} equals {result}");
+    }
+    else 
+    {
+        Console.WriteLine("You did not select a valid number between 1-4.");
+    }
+        //this next line displays the result
+        Console.WriteLine("The result is {0}", result);
+    Console.ReadKey();
+}
 
