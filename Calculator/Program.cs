fce619d500ad020449da263b1f590fc2ba09c244
Matthew Lowry <Lowry-M6@ulster.ac.uk>
2025-09-29 14:54:48 +0100

    edited program code to implement exceptions and try-catch

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,55 +5,61 @@ CalculatorApp();
 
 void CalculatorApp()
 {
-    //declare the variables and initialise them to 0
-    int numOne = 0;
-    int numTwo = 0;
-    int result = 0;
-    int choice = 0;
-    //we will ask the user to input the numbers.
-    //two variables which will store the two numbers for the calc.
-    Console.WriteLine("Type in the 1st number followed by the enter key");
-    numOne = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Type in the 2nd number followed by the enter key");
-    numTwo = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        //promt user to enter first number
+        Console.WriteLine("Enter the first number:");
+        int numOne = Convert.ToInt32(Console.ReadLine());
 
-    //make a loop to make the relevant decision
-    //perform the requested maths operation
-    Console.WriteLine("Choose and option from the following list:");
-    Console.WriteLine("1 - add");
-    Console.WriteLine("2 - subtract");
-    Console.WriteLine("3 - divide");
-    Console.WriteLine("4 - multiply");
-    //convert string to int
-    choice = Convert.ToInt32(Console.ReadLine());
+        //prompt user to enter the second number
+        Console.WriteLine("Enter the second number:");
+        int numTwo = Convert.ToInt32(Console.ReadLine());
 
-    //if statment to perform selected maths op
-    if (choice == 1)
-    {
-        result = numOne + numTwo;
-        Console.WriteLine($"Adding {numOne} and {numTwo} equals {result}");
+        //prompt user to enter the operation
+        Console.WriteLine("Enter the operation (+, -, *, /)");
+
+        /*keyboard is 'string' by defualt
+         * it needs to be switched to char using a switch op
+         */
+        char operation = Convert.ToChar(Console.ReadLine());
+        int result = 0;
+
+        //performing the operation
+        switch (operation)
+        {
+            case '+':
+                result = numOne + numTwo;
+                break;
+            case '-':
+                result = numOne - numTwo;
+                break;
+            case '*':
+                result = numOne * numTwo;
+                break;
+            case '/':
+                result = numOne / numTwo;
+                break;
+            default:
+                Console.WriteLine("invalid operation.");
+                return;
+        }
+        //output the result to the user
+        Console.WriteLine($"Result: {result}");
     }
-    else if (choice == 2)
+    catch (FormatException ex) 
     {
-        result = numOne - numTwo;
-        Console.WriteLine($"Subtracting {numOne} and {numTwo} equals {result}");
+        //here is where the program deals with invalid inputs.
+        Console.WriteLine("Please enter a valid number.");
     }
-    else if (choice == 3)
+    catch (DivideByZeroException ex)
     {
-        result = numOne / numTwo;
-        Console.WriteLine($"Dividing {numOne} and {numTwo} equals {result}");
+        //this handles when the user tries to times by 0
+        Console.WriteLine($"You cannot divide by zero.");
     }
-    else if (choice == 4)
+    finally
     {
-        result = numOne * numTwo;
-        Console.WriteLine($"Multiplying {numOne} and {numTwo} equals {result}");
+       //this runs no matter what
+       Console.WriteLine("Operation completed.");
     }
-    else 
-    {
-        Console.WriteLine("You did not select a valid number between 1-4.");
-    }
-        //this next line displays the result
-        Console.WriteLine("The result is {0}", result);
-    Console.ReadKey();
 }
 
